3530969ac7651a7fa16c94d866eb7e4be1362030
lungnahahd <jyoungjk33@gmail.com>
2023-07-11 01:06:18 +0900

    Add : Lombok test

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,11 +3,15 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
+		log.debug("Hello Lombok Logs!");
 	}
 
 }
